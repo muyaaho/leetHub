6259cb87d72218bcd13f09346519333043f611bf
서지희 <76798969+muyaaho@users.noreply.github.com>
2024-10-02 23:51:58 +0900

    Time: 23 ms (78.54%), Space: 44.5 MB (35.46%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        ArrayList<Integer> dp = new ArrayList<>();
+
+        for (int num : nums) {
+            int idx = findIndex(dp, num);
+            if (dp.contains(num)) continue;
+
+            if (dp.size() <= idx) {
+                dp.add(num);
+            }
+            else {
+                dp.set(idx, num);
+            }
+        }
+        
+        return dp.size();
+    }
+
+    private int findIndex(ArrayList<Integer> arr, int target) {
+        int result = 0;
+        int left = 0, right = arr.size();
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (target < arr.get(mid)) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
